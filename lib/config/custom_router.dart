f6a499120a4beff317ebf8745f9a56fd058f8f0e
linhsol26 <nhglinh.sol@gmail.com>
2021-08-27 16:19:37 +0700

    custom route and splash screen

---
+++ lib/config/custom_router.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/splash/splash_screen.dart';
+
+class CustomRouter {
+  static Route onGeneratedRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+            settings: const RouteSettings(name: '/'),
+            builder: (_) => const Scaffold());
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('Something went wrong!'),
+        ),
+      ),
+    );
+  }
+}
